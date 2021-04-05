da6918c760952b049309b14c26b68f42aa710fb7
dk <871758573@qq.com>
2021-04-05 14:33:27 +0800

    mousewheel 修改

--- src/directives/v-mousewheel.js
+++ src/directives/v-mousewheel.js
@@ -1,18 +1,22 @@
 import normalizeWheel from 'normalize-wheel';
+import {on} from "../utils/dom";
 
 const isFirefox = typeof navigator !== 'undefined' && navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
-const mousewheel = function(element, callback) {
-    if (element && element.addEventListener) {
-        element.addEventListener(isFirefox ? 'DOMMouseScroll' : 'mousewheel', function(event) {
-            const normalized = normalizeWheel(event);
-            callback && callback.apply(this, [event, normalized]);
-        });
-    }
+const bindMousewheel = function (element, callback) {
+    return on(element, isFirefox ? 'DOMMouseScroll' : 'mousewheel', function (event) {
+        const normalized = normalizeWheel(event);
+        callback && callback.apply(this, [event, normalized]);
+    });
 };
 
 export const MouseWheel = {
     bind(el, binding) {
-        mousewheel(el, binding.value);
+        const off = bindMousewheel(el, binding.value);
+        el.__mouseWheelHandle__ = off;
     },
+    unbind(el){
+        el.__mouseWheelHandle__ && el.__mouseWheelHandle__();
+        delete el.__mouseWheelHandle__;
+    }
 };
